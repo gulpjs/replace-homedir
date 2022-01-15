ece0a4e3af6c9aa62b47929504777694d9376181
sttk <sttk.xslet@gmail.com>
2022-01-15 14:30:35 +0900

    Update: Remove dependencies to is-absolute and remove-trailing-separator

--- index.js
+++ index.js
@@ -3,21 +3,18 @@
 var os = require('os');
 var path = require('path');
 
-var isAbsolute = require('is-absolute');
-var removeTrailingSep = require('remove-trailing-separator');
-
 function replaceHomedir(filepath, replacement) {
   if (typeof filepath !== 'string') {
     throw new Error('Path for replace-homedir must be a string.');
   }
 
-  if (!isAbsolute(filepath)) {
+  if (!path.isAbsolute(filepath)) {
     return filepath;
   }
 
-  var home = removeTrailingSep(os.homedir());
+  var home = os.homedir();
   var lookupHome = home + path.sep;
-  var lookupPath = removeTrailingSep(filepath) + path.sep;
+  var lookupPath = path.normalize(filepath + path.sep);
 
   if (lookupPath.indexOf(lookupHome) !== 0) {
     return filepath;
